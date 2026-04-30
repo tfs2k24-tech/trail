6a5541f261177d5c7e289ad62b385ac8295f18ab
kabilesh vs <kabileshvs2005@gmail.com>
2026-04-30 10:39:34 +0530

    fixed vercel mongodb env issue

--- server.js
+++ server.js
@@ -6,25 +6,17 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use("/api/users", userRoutes);
 
-// Health check
-app.get("/", (req, res) => res.send("API is running"));
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-// Connect to MongoDB and start server
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => {
-    console.error("DB connection failed:", err.message);
-    process.exit(1);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("DB connection failed:", err.message));
+
+module.exports = app;
